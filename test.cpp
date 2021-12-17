86c0a42ca5e80f7fe62ac4d85a99ce93acc54834
error404-help <80678444+error404-help@users.noreply.github.com>
2021-12-17 23:56:02 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
-
-int fun(int a, int b);
+#include "Lab1.c"
+//int fun(int a, int b);
 
 TEST(checking_answer, check_option_c){
   
-    EXPECT_EQ(fun(1,1),2);
+    ASSERT_EQ(fun(1,1),2);
 }
