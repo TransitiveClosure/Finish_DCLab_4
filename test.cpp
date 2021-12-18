03406558cd3e91d65400e57ec9267b3cd1ea801d
error404-help <80678444+error404-help@users.noreply.github.com>
2021-12-18 09:23:30 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,12 @@
 #include "Lab1.c"
 //int fun(int a, int b);
 
-TEST(checking_answer, check_option_c){
-  
-    ASSERT_EQ(fun(1,1),2);
+TEST(checking_answer, check_option_c){ 
+    EXPECT_EQ(fun(1,1),2);
+}
+
+int main(int argc, char**argv)
+{
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
