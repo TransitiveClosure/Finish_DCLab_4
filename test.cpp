b2d1674c104d94aa2c88af1f816d750eadd6a71b
error404-help <80678444+error404-help@users.noreply.github.com>
2021-12-18 09:47:28 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,14 +2,44 @@
 #include "Lab1.c"
 //int fun(int a, int b);
 
-TEST(checking_answer, check_option_c){ 
-    EXPECT_EQ(fun(1,1),2);
+using testing::Eq;
+TEST(checking_answer, check_option_c){
+    char *str[3] = { "", "-c", "D:/UNIVER_PROGA/Programing/Programming_labs/TEST.txt"};
+    EXPECT_EQ(work_with_file(3,str),31);
+    str[3] = "--bytes";
+    EXPECT_EQ(work_with_file(3,str),31);
 }
-TEST(checking_answer, check_option_l){ 
-    EXPECT_EQ(fun(2,1),3);
+TEST(checking_answer, check_option_l){
+    char *str[3] = { "", "-l", "D:/UNIVER_PROGA/Programing/Programming_labs/TEST.txt"};
+    EXPECT_EQ(work_with_file(3,str),3);
+    str[3] = "--lines";
+    EXPECT_EQ(work_with_file(3,str),3);
 }
-TEST(checking_answer, check_option_w){ 
-    EXPECT_EQ(fun(5,2),7);
+TEST(checking_answer, check_option_w){
+    char *str[3] = { "", "-w", "D:/UNIVER_PROGA/Programing/Programming_labs/TEST.txt"};
+    EXPECT_EQ(work_with_file(3,str),4);
+    str[3] = "--words";
+    EXPECT_EQ(work_with_file(3,str),4);
+}
+TEST(check_errors, check_error_1001){
+    char *str[2] = { "", "-c"};
+    EXPECT_EQ(work_with_file(2,str),-1001);
+}
+TEST(check_errors, check_error_1002){
+    char *str[3] = { "", "-tgrgsdgsegwg", "D:/UNIVER_PROGA/Programing/Programming_labs/TEST.txt"};
+    EXPECT_EQ(work_with_file(3,str),-1002);
+}
+TEST(check_errors, check_error_1003){
+    char *str[3] = { "", "-l", "D:/UNIVER_PROGA/Programing/Programming_labs/TEST.txtfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"};
+    EXPECT_EQ(work_with_file(3,str),-1003);
+}
+TEST(check_errors, check_error_1004){
+    char *str[3] = { "", "-l", "file.txt"};
+   EXPECT_EQ(work_with_file(3,str),-1004);
+}
+TEST(check_errors, check_error_1005){
+    char *str[3] = { "", "-t", "D:/UNIVER_PROGA/Programing/Programming_labs/TEST.txt"};
+    EXPECT_EQ(work_with_file(3,str),-1005);
 }
 
 int main(int argc, char**argv)
